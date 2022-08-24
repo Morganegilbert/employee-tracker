a6bce0837470e43bde710b825802dd7a4917951d
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-08-23 20:56:53 -0400

    added foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -16,6 +16,9 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INTEGER,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 -- make employee table - need to add foreign key info
@@ -25,4 +28,10 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
   manager_id INTEGER NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
+  FOREIGN KEY (manager_id)
+  REFERENCES manager(id)
+  ON DELETE SET NULL
 );
