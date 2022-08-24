09d3902470992548c162cc1a7c881fabc62ac73c
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-08-23 20:51:24 -0400

    updated schema with info from challenge requirements

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,28 @@
-DROP TABLE IF EXISTS votes;
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS employee_DB;
 
--- change to department
-CREATE TABLE parties (
+CREATE database employee_DB;
+
+USE employee_DBl;
+
+-- make department table - need to add foreign key info
+CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
+  name VARCHAR(50) NOT NULL
+  );
+
+-- make role table - need to add foreign key info
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
 );
 
--- change to role
-CREATE TABLE candidates (
+-- make employee table - need to add foreign key info
+CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party
-    FOREIGN KEY (party_id)
-    REFERENCES parties(id)
-    ON DELETE SET NULL
-);
--- change to employee
-CREATE TABLE votes (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL,
 );
